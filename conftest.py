a374d18d0d850e5cd5cc8d4e21ee6daa694f3717
ConsttsnoC <Constantaasamara@gmail.com>
2023-06-27 19:30:57 +0400

    test add

--- conftest.py
+++ conftest.py
@@ -6,13 +6,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-@pytest.fixture
-def browser():
-    options = Options()
-    options.add_argument("--no-sandbox")
-    options.add_argument("--incognito")
-    options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(options=options)
+@pytest.fixture(params=["chrome", "firefox"])
+def browser(request):
+    if request.param == "chrome":
+        options = Options()
+        options.add_argument("--incognito")
+        options.add_argument("--start-maximized")  # Add this line to maximize the window
+        driver = webdriver.Chrome(options=options)
+    elif request.param == "firefox":
+        options = FirefoxOptions()
+        options.add_argument("-private")
+        driver = webdriver.Firefox(options=options)
+        driver.maximize_window()  # Add this line to maximize the window
+    else:
+        raise ValueError(f"Unsupported browser: {request.param}")
 
     yield driver
 
