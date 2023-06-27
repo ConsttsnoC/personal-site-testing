00ce80e13e983320bddbaae6fc2571ec3226652a
ConsttsnoC <Constantaasamara@gmail.com>
2023-06-27 21:26:39 +0400

    test add

--- conftest.py
+++ conftest.py
@@ -3,20 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
+# Убедитесь, что вы установили colorama
+from colorama import init, Fore
+
+# Инициализация цветовых стилей для консоли
+init(autoreset=True)
+
 @pytest.fixture(params=["chrome", "firefox"])
 def browser(request):
     if request.param == "chrome":
         options = Options()
         options.add_argument("--incognito")
-        options.add_argument("--start-maximized")  # Add this line to maximize the window
-        options.add_argument("--headless")  # Add this line to run in headless mode
+        options.add_argument("--start-maximized")
+        options.add_argument("--headless")
         driver = webdriver.Chrome(options=options)
     elif request.param == "firefox":
         options = FirefoxOptions()
         options.add_argument("-private")
-        options.add_argument("-headless")  # Add this line to run in headless mode
+        options.add_argument("-headless")
         driver = webdriver.Firefox(options=options)
-        driver.maximize_window()  # Add this line to maximize the window
+        driver.maximize_window()
     else:
         raise ValueError(f"Unsupported browser: {request.param}")
 
