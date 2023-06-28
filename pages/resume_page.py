1cfdae774bfb5e7c40465f52a253dd891c6fd0f7
ConsttsnoC <Constantaasamara@gmail.com>
2023-06-28 19:20:53 +0400

    test add

--- pages/resume_page.py
+++ pages/resume_page.py
@@ -1,34 +1,39 @@
 #-> 1) Открытие главной страницы и проверка проверка на наличие резюме после клика по кнопке под фотографией
 # на главной странице
 #-> 2) Проверка на наличие резюме после клика по кнопке под фотографией на главной странице
+from telnetlib import EC
+
+import pytest
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from locators.locat import ButtonsUnderAvatar, NavBar
 from pages.base_page import BasePage
 
 
 class ResumePage(BasePage):
-
+    @pytest.mark.skip(reason="Тест не нужно выполнять")
     def test_click_button_resume(self):
-        '''Проверка наличия резюме после клика по кнопке под фотографией на главной странице'''
-        # Получение идентификатора текущего окна
-        current_window = self.browser.current_window_handle
-
-        # Нажатие на кнопку
-        button = self.browser.find_element(*ButtonsUnderAvatar.RESUME_AVATAR)
+        '''Проверка на наличие резюме после клика по кнопке под фотографией на главной странице'''
+        button_locator = ButtonsUnderAvatar.RESUME_AVATAR
+        button = self.browser.find_element(*button_locator)
         button.click()
 
+        # Явное ожидание загрузки страницы
+        wait = WebDriverWait(self.browser, 10)
+        wait.until(EC.url_contains('.pdf'))  # Ожидаем, что URL содержит '/pdf'
+        self.browser.back()
 
-
-class ResumePageNav(BasePage):
-
+    @pytest.mark.skip(reason="Тест не нужно выполнять")
     def test_click_navbar_resume(self):
-        '''Проверка наличия резюме после клика по кнопке в навигационной панели'''
+
+        '''Проверка на наличие резюме после клика по кнопке под фотографией на главной странице'''
         self.browser.execute_script("window.scrollTo(0, 0)")  # Скроллинг вверх
-        button = self.browser.find_element(*NavBar.RESUME_BUTTON_NAVBAR)
+        button_locator = NavBar.RESUME_BUTTON_NAVBAR
+        button = self.browser.find_element(*button_locator)
         button.click()
-
-
-
-
+        # Явное ожидание загрузки страницы
+        wait = WebDriverWait(self.browser, 10)
+        wait.until(EC.url_contains('.pdf'))  # Ожидаем, что URL содержит '/pdf'
 
 
--- tests/resume_test.py
+++ tests/resume_test.py
@@ -1,15 +1,13 @@
 
-from pages.resume_page import ResumePage,ResumePageNav
+from pages.resume_page import ResumePage
 
 def test_resume(browser):
     link = "https://www.gilmanov.net/"
     page = ResumePage(browser, link)  # инициализируем Page Object с chrome_browser
     page.open()  # открываем страницу
     page.test_click_button_resume()
-    link = "https://www.gilmanov.net/"
-    page = ResumePageNav(browser, link)  # инициализируем Page Object с chrome_browser
-    page.open()  # открываем страницу
     page.test_click_navbar_resume()
+
 
 
 
