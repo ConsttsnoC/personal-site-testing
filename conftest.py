6dce43a0304dde0a72feece6351a6524806ee171
ConsttsnoC <Constantaasamara@gmail.com>
2023-06-28 13:12:09 +0400

    test add

--- conftest.py
+++ conftest.py
@@ -1,28 +1,23 @@
+#Инициализация работы браузеров "chrome" и "firefox", включение режемов инкогнито
+
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
-
-# Убедитесь, что вы установили colorama
-from colorama import init, Fore
-
-# Инициализация цветовых стилей для консоли
-init(autoreset=True)
 
 @pytest.fixture(params=["chrome", "firefox"])
 def browser(request):
     if request.param == "chrome":
         options = Options()
         options.add_argument("--incognito")
-        options.add_argument("--start-maximized")
-        options.add_argument("--headless")
+        options.add_argument("--start-maximized")  # Add this line to maximize the window
         driver = webdriver.Chrome(options=options)
     elif request.param == "firefox":
         options = FirefoxOptions()
         options.add_argument("-private")
-        options.add_argument("-headless")
         driver = webdriver.Firefox(options=options)
-        driver.maximize_window()
+        driver.maximize_window()  # Add this line to maximize the window
     else:
         raise ValueError(f"Unsupported browser: {request.param}")
 
